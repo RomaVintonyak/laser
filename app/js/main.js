3145650b7a987364e0c934caff262102c2e4df3a
Roma Vintonyak <19roman23@gmail.com>
2025-04-02 02:15:42 +0300

    add modal script

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,35 @@
 jQuery(document).ready(function () {
   "use script";
   /*blur text block*/
-  $("#input_fonts").on('input', function(){
+  $("#input_fonts").on('input', function () {
     var inputValue = $(this).val();
     $("._preview").text(inputValue);
   });
-  $("._font").on("click", function(){
+  /*show modal*/
+  $("._font").on("click", function () {
     var fontPreview = $(this).find(".font__preview").text();
     var fontName = $(this).find(".font__name").text();
-    
+    $("#modal").addClass("modal__open");
+    $("body").css({ "overflow": "hidden" });
+    /*write input*/
+    $("#fontLine").val("Надпис:" + fontPreview + "Шрифт: " + fontName);
+    $("._modalPreview").text(fontPreview);
+  });
+  /*modal close*/
+  $("#closeModal").on("click", function () {
+    $("#modal").removeClass("modal__open");
+    $("body").removeAttr("style");
+    $("#fontLine").val("");
+    $("._modalPreview").text("");
+  });
+  /*modal mask close*/
+  $(".modal__mask").on("click", function () {
+    $("#modal").removeClass("modal__open");
+    $("body").removeAttr("style");
+    $("#fontLine").val("");
+    $("._modalPreview").text("");
+  });
+  $(".modal__body").on("click", function (event) {
+    event.stopPropagation();
   });
 });
